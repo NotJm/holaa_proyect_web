93835f13d69a49e2a2ce5c1a655c43d68a1b85b0
JoseMariaGarciaMendoza <20221065@uthh.edu.mx>
2024-10-21 21:28:22 -0600

    Loading Animation

--- src/app/components/loading/loading.component.ts
+++ src/app/components/loading/loading.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-loading',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './loading.component.html',
   styleUrl: './loading.component.css'
 })
 export class LoadingComponent {
+  isFadingOut = false;
 
+  onAnimationEnd() {
+    setTimeout(() => {
+      this.isFadingOut = true; 
+    }, 100); 
+  }
 }
